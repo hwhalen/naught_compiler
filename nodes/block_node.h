cc8e902a20fd00f0267572ba5ee42b0097af61e8
hwhalen <hjwhalen825@gmail.com>
2013-11-30 02:52:33 -0800

    continued improvement of test2 compilation and the printing methods for many of the nodes

---
+++ nodes/block_node.h
@@ -0,0 +1,41 @@
+#ifndef _BLOCK_NODE_H
+#define _BLOCK_NODE_H
+
+#include <iostream>
+#include <vector>
+
+#include "vardecl_node.h"
+#include "stmt_node.h"
+
+using std::ostream;
+using std::vector;
+
+class block_node {
+  public:
+    block_node(vector<vardecl_node> v, vector<stmt_node> s) {
+      var_decs = v;
+      stmts = s;
+    }
+
+    friend ostream& operator<<(ostream &os, const block_node &obj) {
+      return (obj.printHelper(os));
+    }
+
+  private:
+    vector<vardecl_node> var_decs;
+    vector<stmt_node> stmts;
+
+    virtual ostream& printHelper(ostream &os) const {
+      os << "var_decs={";
+      for (size_t i = 0; i < var_decs.size(); i++) {
+        os << (var_decs[i]);
+      }
+      os << "} and stmts={";
+      for (size_t i = 0; i < stmts.size(); i++) {
+        os << (stmts[i]);
+      }
+      os << "}";
+      return os;
+    }
+};
+#endif // _BLOCK_NODE_H
